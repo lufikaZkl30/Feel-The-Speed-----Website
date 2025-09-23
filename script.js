5556b39664147d1070b37ad1cf2d5283caf8cbb6
lufikaZkl30 <lufika.535220223@stu.untar.ac.id>
2025-09-23 10:01:19 +0700

    Penambahan code

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script sederhana untuk interaksi
+document.addEventListener("DOMContentLoaded", () => {
+  const cta = document.querySelector(".cta");
+  cta.addEventListener("click", () => {
+    alert("Let's create your vision together!");
+  });
+});
